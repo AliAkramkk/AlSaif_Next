f1cdce4e26c79156c12c55818b252674c9491c77
clientsmaker <interiorclientsmaker@gmail.com>
2025-05-28 12:50:02 +0530

    modified the image path

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+     <Head>
+        {/* ✅ Favicon */}
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+        {/* Optional: support more formats */}
+        <link rel="icon" type="image/png" href="/assets/logo.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
